255e11c74563b64fa5abbdba8e8035cdd74b2c45
Samuel Trezitorul <trezitorul@gmail.com>
2015-03-04 02:33:27 -0600

    Made some demo tests to demonstrate aspects of the machine are working and updated logs

--- PullTester.cpp
+++ PullTester.cpp
@@ -1,5 +1,6 @@
 #include <PropWare/printer/printer.h>
 #include <PropWare/mcp3000.h>
+#include <simpletools.h>
 
 using namespace PropWare;
 
@@ -7,14 +8,18 @@ const Port::Mask SCLK = Port::P29;
 const Port::Mask MISO = Port::P28;
 const Port::Mask MOSI = Port::P27;
 const Port::Mask CS   = Port::P26;
+const Port::Mask MO   = Port::P23;
+const Port::Mask M1   = Port::P22;
 
 const MCP3000::Channel    CHAN    = MCP3000::CHANNEL_0;
 const MCP3000::PartNumber PART_NO = MCP3000::MCP320x;
-
+int motorControlDemo();
+int buttonDemo();
 int main () {
 	ErrorCode errorCode;
 	uint16_t  data;
-
+	buttonDemo();
+	motorControlDemo();
 	pwOut.printf("Hello World-Initializing ADC NOW" CRLF);
 	SPI     *spi = SPI::get_instance();
 	MCP3000 adc(spi, PART_NO);
@@ -29,4 +34,33 @@ int main () {
 	}
 
 	return 0;
+}
+
+int motorControlDemo(){
+	pwOut.printf("Hello World-Initializing Motor Control Demo Now" CRLF);
+	for(int i=1;i<11;i++){
+		high(23);
+		pwOut.printf("Going up for the # %d time" CRLF, i);
+		waitcnt(5*SECOND + CNT);
+		low(23);
+		high(22);
+		pwOut.printf("Goind down for the # %d time" CRLF,i);
+		waitcnt(5*SECOND + CNT);
+		low(22);
+	}
+	pwOut.printf("Motor Demo Complete" CRLF);
+	return 0;
+}
+
+int buttonDemo(){
+	pwOut.printf("ButtonDemo");
+	int left;
+	int right;
+	while(1){
+		left=input(25);
+		right=input(24);
+		pwOut.printf("The Left button is %d" CRLF, left);
+		pwOut.printf("The Right button is %d" CRLF, right);
+		waitcnt(SECOND + CNT);
+	}
 }
